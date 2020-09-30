a1886932f95cb567a57b30a41ed06d9b7bc354df
John Whitaker <johnwhitakeraustin@gmail.com>
2020-09-30 16:46:31 -0700

    LVL 2 pwn3d

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,7 +1,8 @@
 // Establish Variables
 let tableData = data,
     button = d3.select("#filter-btn"),
-    reset = d3.select("#reset-btn")
+    reset = d3.select("#reset-btn"),
+    filreset = d3.select("#filreset-btn")
     form = d3.select("form"),
     tbody = d3.select("tbody");
 
@@ -24,28 +25,54 @@ const runFilter = () => {
     // Prevent refresh on submit
     d3.event.preventDefault();
 
-    // Grab filter input and retrieve relevent data 
-    let inputElement = d3.select("#datetime"),
-        inputValue = inputElement.property("value");
-
-    // Check output and filter value
-    console.log(tableData);
-    console.log(inputValue);
-
+    // Date filter
+    let dateData = data,
+        dateFilter = d3.select("#datefil"),
+        dateValue = dateFilter.property("value");
+    if (dateValue != "") {
+        dateData = tableData.filter(sighting => sighting.datetime.includes(dateValue));
+    } 
+    // City filter
+    let cityData = dateData,
+        cityFilter = d3.select("#cityfil"),
+        cityValue = cityFilter.property("value");
+    if (cityValue != "") {
+        cityData = dateData.filter(sighting => sighting.city.includes(cityValue));
+    }
+    // State filter
+    let stateData = cityData,
+        stateFilter = d3.select("#statefil"),
+        stateValue = stateFilter.property("value");
+    if (stateValue != "") {
+        stateData = cityData.filter(sighting => sighting.state.includes(stateValue));
+    }
+    // Country filter
+    let countryData = stateData,
+        countryFilter = d3.select("#countryfil"),
+        countryValue = countryFilter.property("value");
+    if (countryValue != "") {
+        countryData = stateData.filter(sighting => sighting.country.includes(countryValue));
+    }
+    // Shape filter
+    let shapeData = countryData,
+        shapeFilter = d3.select("#shapefil"),
+        shapeValue = shapeFilter.property("value");
+    if (shapeValue != "") {
+        shapeData = countryData.filter(sighting => sighting.shape.includes(shapeValue));
+    }
     // Assign filter to data & check output
+    let filteredData = shapeData
+    console.log(filteredData)
 
 
     // Clear table and loop to assign filtered values
     tbody.html("");
-    data.forEach((sighting) => {
-        if ((sighting.datetime == inputValue)
-         && (sighting.city == "seattle")) {
-            let row = tbody.append("tr");
-            Object.values(sighting).forEach(value => {
-                let cell = row.append("td");
-                cell.text(value);
-            })
-        }
+    filteredData.forEach((sighting) => {
+        let row = tbody.append("tr");
+        Object.values(sighting).forEach(value => {
+            let cell = row.append("td");
+            cell.text(value);
+        })
     })
 }
 
@@ -61,7 +88,12 @@ const runReset = () => {
     })
 }
 
+const runFilreset = () => {
+    location.reload(true);
+}
+
 // Event handlers
 button.on("click", runFilter);
 form.on("submit",runFilter);
 reset.on("click", runReset);
+filreset.on("click",runFilreset);
